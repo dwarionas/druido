f754e236de4fdc4bf07a4915042896d3d09e3cbf
Ihor <dwarionas@gmail.com>
2025-09-10 18:16:07 +0200

    feat: add language and tags to deck model

--- server/src/models/Deck.ts
+++ server/src/models/Deck.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const deckSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: String,
+    language: { type: String, default: 'UK' },
+    tags: [String],
     userId: { type: String, required: true },
 }, { timestamps: true });
 
