17d2900bd38263ac0af3bbb471e55c6ba5944e99
Ihor <dwarionas@gmail.com>
2026-02-22 21:40:45 +0100

    fix: mobile requests

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
         return [
             { source: "/login/:path*", destination: "/login/:path*" },
             { source: "/app/:path*", destination: "/app/:path*" },
+            { source: "/api/:path*", destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/:path*` },
         ];
     },
     reactStrictMode: false
